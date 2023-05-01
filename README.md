6389f2de59bab4f9c77c0b4075362f38bb30b7f7
youliang <tan_you_liang@hotmail.com>
2023-05-01 18:09:42 -0400

    wip newsgpt deployed with newsgpt lib
    Signed-off-by: youliang <tan_you_liang@hotmail.com>

--- README.md
+++ README.md
@@ -6,4 +6,35 @@ cloud function implementation for newsgpt site
 - Medium post on azure openai langchai: https://medium.com/microsoftazure/azure-openai-and-langchain-eba69f18f050
 - Azure OpenAI service: https://learn.microsoft.com/en-gb/azure/cognitive-services/openai/quickstart?tabs=command-line&pivots=programming-language-python
 - Langchain summarized docs: https://python.langchain.com/en/latest/modules/chains/index_examples/summarize.html
-- Azure function: https://learn.microsoft.com/en-us/azure/azure-signalr/signalr-quickstart-azure-functions-python
+- Azure function: https://learn.microsoft.com/en-us/azure/azure-functions/create-first-function-cli-python
+
+# Run server locally
+
+```bash
+# terminal 1
+azurite
+
+# terminal 2
+func start
+```
+
+Open UI: http://localhost:7071/api/index
+
+# Deploy to cloud
+
+After testing the functions locally, start the deployment to the cloud by following the steps in [this link](https://learn.microsoft.com/en-us/azure/azure-functions/create-first-function-cli-python?tabs=azure-cli%2Cbash&pivots=python-mode-decorators#create-supporting-azure-resources-for-your-function)
+
+In our setup:
+ - <RESOURCE_GROUP_NAME>: newsgpt
+ - <APP_NAME>: newsgpt-ai
+
+
+Lastly deploy the function to the cloud with the following command:
+
+```bash
+# Update app settings
+az functionapp config appsettings set --name newsgpt-ai --resource-group newsgpt --settings AzureWebJobsFeatureFlags=EnableWorkerIndexing
+
+# then deploy
+func azure functionapp publish  newsgpt-ai
+```
--- function_app.py
+++ function_app.py
@@ -3,31 +3,43 @@ This uses v2 api
 https://learn.microsoft.com/en-us/azure/azure-functions/functions-reference-python?tabs=asgi%2Capplication-level&pivots=python-mode-decorators
 """
 
+import datetime
 import azure.functions as func
+import logging
 import os
 import time
 import json
+from newsgpt import NewsGPT
 
 app = func.FunctionApp()
 
+##############################################################################
+
+
 @app.function_name(name="get_last_update")
-@app.route(route="last_update") # HTTP Trigger
+@app.route(route="last_update", auth_level=func.AuthLevel.ANONYMOUS)
 def get_last_update(req: func.HttpRequest) -> func.HttpResponse:
     time_now_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    # TODO: get this last update time from the database
     return func.HttpResponse(time_now_str)
 
+##############################################################################
+
+
 @app.function_name(name="get_static")
-@app.route(route="index")
+@app.route(route="index", auth_level=func.AuthLevel.ANONYMOUS)
 def get_static(req: func.HttpRequest) -> func.HttpResponse:
     f = open(os.path.dirname(os.path.realpath(__file__)) + '/index/index.html')
     return func.HttpResponse(f.read(), mimetype='text/html')
 
+##############################################################################
+
 
 @app.function_name(name="submit_news_form")
-@app.route(route="submit_news_form") # HTTP Trigger
+@app.route(route="submit_news_form", auth_level=func.AuthLevel.ANONYMOUS)
 def submit_news_form(
-        req: func.HttpRequest,
-    ) -> func.HttpResponse:
+    req: func.HttpRequest,
+) -> func.HttpResponse:
     # receives a POST request with a JSON body and return a json response
     req_body = req.get_json()
 
@@ -36,10 +48,13 @@ def submit_news_form(
 
     if req_body:
         json_res["headlines"] = \
-            f" Getting headlines for {req_body['category']}, {req_body['region']}, {req_body['length']} "
+            f" Getting headlines for {req_body['category']}, {req_body['region']}, {req_body['length']} \n " \
+            f" HA, I am still a work in progress."
+
+        # TODO: get the headlines from the database
 
         return func.HttpResponse(
-            body = json.dumps(json_res),
+            body=json.dumps(json_res),
             status_code=200
         )
     else:
@@ -48,3 +63,28 @@ def submit_news_form(
             body=json.dumps(json_res),
             status_code=400
         )
+
+##############################################################################
+# Cron job to update the database from langchain
+# https://learn.microsoft.com/en-us/azure/azure-functions/functions-bindings-timer?tabs=python-v2%2Cin-process&pivots=programming-language-python
+
+
+# TODO: this isnt working, dunno why need further debugging
+@app.function_name(name="mytimer")
+@app.schedule(schedule="0 */5 * * * *", 
+              arg_name="mytimer",
+              run_on_startup=True) 
+def test_function(mytimer: func.TimerRequest) -> None:
+    logging.info('Python timer trigger function')
+    # TODO: update the database from langchain
+    _api_key = "700fa82411ad46069807d49abd48c7ad"
+    _api_base = "https://newsgpt.openai.azure.com/"
+    _api_type = 'azure'
+    _api_version = '2022-12-01'
+    _model_name = "text-davinci-003"
+
+    newsgpt = NewsGPT(api_key=_api_key,
+                      api_type=_api_type,
+                      api_base=_api_base,
+                      api_version=_api_version,
+                      model_name=_model_name)
